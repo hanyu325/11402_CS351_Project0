6d03942622ff86962cb65f1dcc53924c0015f9fd
hanyu325 <hanyu0325@gmail.com>
2026-05-13 13:49:31 +0800

    BF11402-14, Add CMake and TwoSum Test Cases

---
+++ src/TwoSum.h
@@ -0,0 +1,11 @@
+// src/TwoSum.h
+#ifndef TWOSUM_H
+#define TWOSUM_H
+
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> TwoSumHashTable(std::vector<int>& nums, int target);
+std::vector<int> TwoSumArray(std::vector<int>& nums, int target);
+
+#endif
